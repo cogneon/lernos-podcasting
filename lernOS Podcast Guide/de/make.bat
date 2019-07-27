498f31ca318bd8d30f0277764caf339173d3a4d1
Simon Dückert <simon.dueckert@cogneon.de>
2019-07-27 19:21:32 +0200

    Version 0.2 with podcasting sketchnote

--- lernOS Podcast Guide/de/make.bat
+++ lernOS Podcast Guide/de/make.bat
@@ -25,8 +25,14 @@ REM Create PDF Version (pdf)
 pandoc %filename%.md metadata/metadata.yaml -o %filename%.pdf --from markdown --template lernOS --number-sections -V lang=de-de
 
 REM Create eBook Versions (epub, mobi)
-magick -density 300 lernOS-Podcasting-Guide-de.pdf[0] images/ebook-cover.png
-pandoc -s --epub-cover-image=images/ebook-cover.png -o %filename%.epub %filename%.md
+REM magick -density 300 %filename%.pdf[0] images/ebook-cover.png
+REM pandoc -s --epub-cover-image=images/ebook-cover.png -o %filename%.epub %filename%.md
+REM ebook-convert %filename%.epub %filename%.mobi
+
+magick -density 300 %filename%.pdf[0] images/ebook-cover.jpg
+magick mogrify -size 2500x2500 -resize 2500x2500 images/ebook-cover.jpg
+magick mogrify -crop 1563x2500+102+0 images/ebook-cover.jpg
+pandoc %filename%.md metadata/metadata.yaml -s --epub-cover-image=images/ebook-cover.jpg -o %filename%.epub
 ebook-convert %filename%.epub %filename%.mobi
 
 pause
